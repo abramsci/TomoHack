eeb1293b49ee04001a71b52322d892a0d4327253
Sergei Abramenkov <6279507+abramsci@users.noreply.github.com>
2022-10-09 17:50:41 +0700

    Revive changes from the last day of GeoHack

--- src/ray_tracing.py
+++ src/ray_tracing.py
@@ -10,12 +10,12 @@
 # Python standard library imports
 import sys
 import math
-import numpy as np
 
-# Necessary packages (not a part of standard library)
-from matplotlib import pyplot as plt
+# Necessary packages
+import numpy
 
 # Project modules (in the same folder/repo)
+from velocity_model import Model2D
 from notes import __status__, __email__, __maintainer__, __credits__
 
 ############################### GLOBAL CONSTANTS #############################
@@ -24,56 +24,81 @@ __copyright__ = "Copyright 2022, NSU GeoHack"
 __license__   = "MIT"
 __version__   = "0.0.2"
 
+
+H = 1.0
+TEST_MODEL = Model2D(numpy.array([[1, 2, 3, 4, 5], 
+                                [6, 7, 8, 9, 10],
+                                [11, 12, 13, 14, 15]]),
+                                h=H, name='testmodel')
+"""Test velocity model:
+
+          0   1   2   3   4 
+        +---+---+---+---+---+
+        |111|222|333|444|555|
+      0 |111|2S2|333|444|555| h
+        |111|222|333|444|555|
+        +---+---+---+---+---+
+        |666|777|888|999|AAA|
+      1 |666|777|888|999|AAA| h
+        |666|777|888|999|AAA|
+        +---+---+---+---+---+
+        |BBB|CCC|DDD|EEE|FFF|
+      2 |BRB|CCC|DDD|EEE|FFF| h
+        |BBB|CCC|DDD|EEE|FFF|
+        +---+---+---+---+---+
+          h   h   h   h   h
+"""
+
+S = (0, 1)
+R = (2, 0)
+
+
 ############################# CLASSES & FUNCTIONS ############################
 
-velmod = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-h = 1.0
-
-source = (0, 1)
-receiver = (2, 0)
-
-
-Class Ray2D()
-    def __init__(self, source, receiver):
-        self.time = None
-        self.source
-        self.receiver
-        
+class Ray2D():
     
+    def __init__(self, source_pos: int, receiver_pos: int):
+        self.s = source_pos
+        self.r = receiver_pos
+        self.t = None 
+        
+    def trace(self, model: Model2D):
+        """Trace itself in some velocity model"""
+        # Obtaining straight line equation
+        a = (self.s[1] - self.r[1]) / (self.s[0] - self.r[0])
+        b = model.h * (self.s[1] + 0.5 - a * (self.s[0] + 0.5))
+        print(f"y = {a}x + {b}")
+        print(f"x = (y - {b}) / {a}")
 
-def trace(self, velmod):
-    # Obtaining straight line equation
-    a = (source[1] - receiver[1]) / (source[0] - receiver[0])
-    b = h * (source[1] + 0.5 - a * (source[0] + 0.5))
-    print(f"y = {a}x + {b}")
-    print(f"x = (y - {b}) / {a}")
+        # Looking for points on block edges
+        points = []
+        i_start = min(self.s[0], self.r[0]) + 1
+        i_end = max(self.s[0], self.r[0]) + 1
+        for i in range(i_start, i_end):
+            points.append((i* model.h, a * i * model.h + b))
+        print(points)
+        j_start = min(self.s[1], self.r[1]) + 1
+        j_end = max(self.s[1], self.r[1]) + 1
+        for j in range(j_start, j_end):
+            points.append(((j * model.h - b) / a, j * model.h))
 
-    # Looking for points on block edges
-    points = []
-    i_start = min(source[0], receiver[0]) + 1
-    i_end = max(source[0], receiver[0]) + 1
-    for i in range(i_start, i_end):
-        points.append((i * h, a * i * h + b))
-    j_start = min(source[1], receiver[1]) + 1
-    j_end = max(source[1], receiver[1]) + 1
-    for j in range(j_start, j_end):
-        points.append(((j * h - b) / a , j * h))
+        points.append(((self.s[0] + 0.5) * model.h, 
+                        (self.s[1] + 0.5) * model.h))
+        points.append(((self.r[0] + 0.5) * model.h,
+                        (self.r[1] + 0.5) * model.h))
+        points.sort()
+        print(points)
 
-    points.append(((source[0] + 0.5) * h, (source[1] + 0.5) * h))
-    points.append(((receiver[0] + 0.5) * h, (receiver[1] + 0.5) * h))
-    points.sort()
-    print(points)
-
-    #Counting time of a ray arrival
-    t_sum = 0
-    for k in range(len(points) - 1):
-        print(points[k], points[k + 1])
-        s = math.sqrt((points[k + 1][0] - points[k][0]) ** 2
+        #Counting time of a ray arrival
+        t_sum = 0
+        for k in range(len(points) - 1):
+            print(points[k], points[k + 1])
+            s = math.sqrt((points[k + 1][0] - points[k][0]) ** 2
                         + (points[k + 1][1] - points[k][1]) ** 2)
-        i = int((points[k + 1][0] + points[k][0])/2)
-        j = int((points[k + 1][1] + points[k][1])/2)
-        t_sum = t_sum + s / velmod[i, j]
-        print(f'Segment distance = {s}; velocity = {velmod[i,j]} t_sum = {t_sum}')
+            i = int((points[k + 1][0] + points[k][0])/2)
+            j = int((points[k + 1][1] + points[k][1])/2)
+            t_sum = t_sum + s / model[j, i]
+            print(f'Segment = {s}; velocity = {model[j,i]} t_sum = {t_sum}')
 
 
 
@@ -83,6 +108,16 @@ if __name__ == '__main__':
     print(f'Supplied {len(sys.argv)} command line arguments.')
     for i, arg in enumerate(sys.argv):
         print(f'Command line argument {i}: {arg}')
+    if len(sys.argv) == 5:
+        s = (int(sys.argv[1]), int(sys.argv[2]))
+        r = (int(sys.argv[3]), int(sys.argv[4]))
+    else:
+        s = (S[0], S[1])
+        r = (R[0], R[1])
+    ray = Ray2D(s, r)
+    print(ray.t)
+    print(TEST_MODEL)
+    ray.trace(TEST_MODEL)
     print(f'Exiting script {__file__} without errors.')
     exit(0)
 
--- src/velocity_model.py
+++ src/velocity_model.py
@@ -12,10 +12,11 @@ Python Image Library (PIL) is used for picture reading and drawing.
 
 # Python standard library imports
 import sys
+from pathlib import Path
 
 # Necessary packages
+import numpy
 from PIL import Image
-from numpy import asarray
 
 # Project modules
 from notes import __status__, __email__, __maintainer__, __credits__
@@ -27,28 +28,88 @@ __copyright__ = "Copyright 2022, NSU GeoHack"
 __license__   = "MIT"
 __version__   = "0.0.2"
 
-# Velocity limitations for a model
-V_MIN = 0.3 # km/s
-V_MAX = 2.3 # km/s
+# Default velocity model limitations 
+V_MIN: float = 0.3 # km/s
+V_MAX: float = 2.3 # km/s
 
 
 ############################# CLASSES & FUNCTIONS ############################
 
-"""Read image, change colorspace to grayscale and make NumPy array from it.
-"""
-def read_picture(path):
-    image = Image.open(path) 
-    gray_scale = image.convert(mode='L')
-    image.show()
-    gray_scale.show()
-    print(image.mode)
-    print(gray_scale.mode)
+class Model2D(numpy.ndarray):
+    """A pixelized velocity model.
+
+    Represents quite simple blocky structure of the media in 2D.
+    Each block is a square with a constant velocity inside its borders.
+    One can set some Xmin/Xmax and Ymin/Ymax as the limits of the model.
+    Instead we use h - an arbitrary length of the square block side.
+    Therefore:  Xmin = 0, Xmax = h * model.shape[0]
+                Ymin = 0, Ymax = h * model.shape[1]
+    Adjusting model to fit some arbitrary Xmin/Xmax and Ymin/Ymax is trivial.
+    Changing h allows easy scaling of the model to fit different dataset,
+    as long as we maintain fixed aspect ratio based on the model shape.
+
+          0   1   2   3   4   5   6   7   8   9
+        +---+---+---+---+---+---+---+---+---+---+
+        |111|333|222|444|555|444|333|222|444|555|
+      0 |111|333|222|444|555|444|333|222|444|555| h
+        |111|333|222|444|555|444|333|222|444|555|
+        +---+---+---+---+---+---+---+---+---+---+
+        |222|333|222|444|555|444|333|222|444|555|
+      1 |222|333|222|444|555|444|333|222|444|555| h
+        |222|333|222|444|555|444|333|222|444|555|
+        +---+---+---+---+---+---+---+---+---+---+
+        |444|333|222|444|555|444|333|222|444|555|
+      2 |444|333|222|444|555|444|333|222|444|555| h
+        |444|333|222|444|555|444|333|222|444|555|
+        +---+---+---+---+---+---+---+---+---+---+
+          h   h   h   h   h   h   h   h   h   h
+
+    With such representation Model2D class can be an extention of numpy.ndarray.
+    We only add couple additional attributes:
+        Model2D.name: str - a name for the model
+        Model2D.h: float - scaling factor (block size)
+    """
+    def __new__(cls, input_array, h=1.0, name=None):
+        """Normally we would have our model as a standard NumPy array.
+        First check if it is two-dimensional (2D) by checking len(array.shape)
+        Then if we good, simply casting our Model2D class from it.
+        And finally adding model attributes like name and scale.
+        """
+        obj = numpy.asarray(input_array).view(cls)
+        obj.h = h               # Model scale (see class docstring)
+        obj.name = name         # Model name (string to identify it by human)
+        return obj
+
+    def __array_finalize__(self, obj):
+        """Provide a finalization method for class to work properly"""
+        if obj is None: return
+        self.h = getattr(obj, 'h', None)
+        self.name = getattr(obj, 'name', None)
     
-    velmod = asarray(gray_scale)
-    print(type(velmod))
-    velmod = velmod / 255 * (V_MAX - V_MIN) + V_MIN     
-    print(velmod)
-    return(velmod)
+    @classmethod
+    def from_image(cls, image: Image, vmin=V_MIN, vmax=V_MAX):
+        """Convert image to grayscale and creating model from it.
+        An alternative constructor of class instance.
+        More about @classmethod decorator:
+            https://www.youtube.com/watch?v=rq8cL2XMM5M
+        """
+        #print(type(image))
+        gray_scale = image.convert(mode='L')
+        velmod = numpy.asarray(gray_scale) / 255 * (vmax - vmin) + vmin
+        print(type(velmod))
+        print(velmod.shape)
+        return Model2D(velmod)
+    
+
+def read_image_as_model(path: Path):
+    try:
+        image = Image.open(path)
+    except FileNotFoundError:
+        pass
+    #image.show()
+    return Model2D.from_image(image)
+
+#def write_model_as_image(model: Model2D, path: Path, colorscale='gray'):
     
 
 ############################### SCRIPT BEHAIVIOR #############################
@@ -57,6 +118,10 @@ if __name__ == '__main__':
         print(f'Command line argument {i}: {arg}')
     if len(sys.argv) == 2:
         print(f'Reading {sys.argv[1]} picture as a velocity model')
-        read_picture(sys.argv[1])
+        model = read_image_as_model(Path(sys.argv[1]))
+        print(type(model))
+        print(model.name)
+        print(model.h)
+        print(model)
     print(f'Exiting script {__file__} without errors.')
     exit(0)
